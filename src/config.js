31f072832fa6e32424fb9b5f3e22fd6e00ea2a4f
Ramil Zaynetdinov <ramil.zv@gmail.com>
2020-07-08 14:24:37 +0300

    updated peer server config

--- src/config.js
+++ src/config.js
@@ -30,9 +30,9 @@ let config = {
   },
 
   peerJsServer: {
-    host: '0.peerjs.com',
-    port: 443,
-    path: '/',
+    host: 'localhost',
+    port: 9000,
+    path: '/conference',
     debug: 0, // 0 - disable logs, 1 - only errors, 2 - errors and warnings, 3 - all logs
   },
 };
